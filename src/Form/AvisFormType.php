d3007cc4f2a2971b01c799a0d8cff69804ad54d2
Yordle38 <97458919+Yordle38@users.noreply.github.com>
2024-04-04 16:32:05 +0200

    image par défaut d'acteur ok (plus belle la vie)

--- src/Form/AvisFormType.php
+++ src/Form/AvisFormType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Validator\Constraints\Range;
 
 class AvisFormType extends AbstractType
 {
@@ -16,8 +18,11 @@ class AvisFormType extends AbstractType
             ->add('idMovie', HiddenType::class)
             ->add('idSerie', HiddenType::class)
             ->add('username')
-            ->add('rates')
-            ->add('description')
+            ->add('rates', IntegerType::class, [
+                'constraints' => [
+                    new Range(['min' => 0, 'max' => 10]), // Contrainte de validation pour que le rate soit entre 0 et 10
+                ],
+            ])            ->add('description')
         ;
     }
 
